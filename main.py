af487ecc1166338663fc7e75a9684afc0eb8f726
Anthony Lopez <51010799+tonistroke@users.noreply.github.com>
2022-08-22 02:16:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 from pandas_datareader import wb
+import mathplotlib.pyplot as plt
+
 
 Immunization_measles = "SH.IMM.MEAS"
 Immunization_DPT = "SH.IMM.IDPT"
 
 Panama = ["PAN"]
 
-wb.download(indicator = Immunization_measles, country = Panama, start = 2018, end = 2018)
+df1 = wb.download(indicator = Immunization_measles, country = Panama, start = 1980, end = 2018)
+
+df2 = wb.download(indicator = Immunization_DPT, country = Panama, start = 2010, end = 2018)
+
+df = df1.merge(df2, on=country)
+
+df.columns = ["año", "Porsentaje de niños (%)"]
+df["Porsentaje de niños (%)"].plot(kind="bar")
